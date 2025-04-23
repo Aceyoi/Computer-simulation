ad62118acabb1d851a76935163da32f9a47c9312
Aceyoi <146310875+Aceyoi@users.noreply.github.com>
2025-04-23 11:09:22 +0900

    Add files via upload

---
+++ Лабораторная работа 1/Buffon-s-problem-master/Buffon's problem/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Buffon_s_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(735, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(208, 84);
+            button1.TabIndex = 0;
+            button1.Text = "Выполнить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(118, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 15);
+            label1.TabIndex = 3;
+            label1.Text = "количество экспериментов";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(280, 15);
+            label2.TabIndex = 4;
+            label2.Text = "количество бросков иглы в одном эксперименте";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 143);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(705, 364);
+            listBox1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(735, 484);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(208, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(735, 466);
+            label3.Name = "label3";
+            label3.Size = new Size(121, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Среднее отклонение";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(401, 113);
+            label4.Name = "label4";
+            label4.Size = new Size(316, 15);
+            label4.TabIndex = 8;
+            label4.Text = "3,14159265358979323846264338327950288419716939937510";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(955, 519);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Лабораторная работа 1/Buffon-s-problem-master/Buffon's problem/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Forms;
+
+namespace Buffon_s_problem
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double buffon_dpi(int N, int M)
+        {
+            listBox1.Items.Clear();
+            double l = 1.0;
+            double a = 2.0;
+            List<double> md = new List<double>(M); // Список для хранения отклонений
+
+            Random random = new Random();
+
+            for (int j = 0; j < M; ++j)
+            {
+                int sum_p = 0;
+                for (int i = 0; i < N; ++i)
+                {
+                    double y = random.NextDouble() * a; // Случайное расстояние до линии
+                    double q = random.NextDouble() * Math.PI; // Случайный угол
+                    if (y <= l * Math.Sin(q))
+                    {
+                        sum_p++;
+                    }
+                    double mpi = (double)N / (sum_p == 0 ? 1 : sum_p); // Оценка числа π 
+                    double deviation = Math.Abs(mpi - Math.PI); // Отклонение от эталона
+                    listBox1.Items.Add($"В {j + 1} эксперименте на {i + 1} попытке броска получилось число {mpi} с отклонением: {deviation:F6}");
+                }
+
+                double finalMpi = (double)N / sum_p; // Оценка числа π
+                md.Add(Math.Abs(finalMpi - Math.PI)); // Отклонение от эталона
+            }
+
+            double d = 0.0;
+            for (int j = 0; j < M; ++j)
+            {
+                d += md[j];
+            }
+            d /= M;
+            return d;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(textBox1.Text);
+            int M = int.Parse(textBox2.Text);
+
+            double result = buffon_dpi(N, M);
+
+            textBox3.Text = result.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
